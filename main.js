91326f62769630988f45db039ca0d49e7d170e84
Camila Valim Dias <camila.valim2004@gmail.com>
2025-02-21 14:05:17 -0300

    Chore: dividindo o event listener em funções para organizar o Código JS.

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const form = document.getElementById('form-atividade');
-let linhas = '';
 const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando">';
 const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji triste">';
-
+let linhas = '';
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha(){
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
     
@@ -18,10 +22,11 @@ form.addEventListener('submit', function(e){
 
     linhas +=linha;
 
-    const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linhas;
-
     inputNomeAtividade.value= '';
     inputNotaAtividade.value='';
+}
 
-});
+function atualizaTabela(){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
